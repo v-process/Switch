84cbf87360d837609b8475172b449320b60df71d
hyunj0525 <ghyunj0525@gmail.com>
2015-05-20 03:05:44 +0900

    지도액티비티 추가

--- SwitchSource/ONOFFZONE/app/src/main/java/com/androidbegin/onoffzone/MapActivity.java
+++ SwitchSource/ONOFFZONE/app/src/main/java/com/androidbegin/onoffzone/MapActivity.java
@@ -1,7 +1,54 @@
 package com.androidbegin.onoffzone;
 
-/**
- * Created by WFUser on 2015-05-20.
- */
-public class MapActivity {
+        import android.os.Bundle;
+        import android.support.v7.app.ActionBarActivity;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.MapFragment;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.Marker;
+        import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends ActionBarActivity {
+
+    static final LatLng SEOUL = new LatLng( 37.56, 126.97);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_map);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        //Marker seoul = map.addMarker(new MarkerOptions().position(SEOUL.title("Seoul"));타이틀 부분에 오류
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
+
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
